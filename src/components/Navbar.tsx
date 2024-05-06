bf1bb1ad74a1a441c0fa91ae86e08d7ce6a4d783
akash <akashsonkamble199@gmail.com>
2024-05-06 17:58:49 +0530

    style: Update Tailwind CSS in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useScrollEffect } from "@/hooks/useScroll";
 import Link from "next/link";
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -32,56 +33,60 @@ const navLinks = [
 /**
  * Renders the Navbar component.
  *
- * @param {object} props The component's props.
- * @param {React.ReactNode} props.children The component's children.
- * @returns {JSX.Element} The Navbar component.
+ * @param props The component's props.
+ * @param props.children The component's children.
+ * @returns The Navbar component.
  */
 const Navbar: React.FC<{
     children?: React.ReactNode;
-}> = ({ children }: { children?: React.ReactNode }): JSX.Element => {
-    const [openMobileMenu, setOpenMobileMenu] = useState<boolean>(false);
+}> = ({ children }: {
+    children?: React.ReactNode;
+}): JSX.Element => {
+        const [openMobileMenu, setOpenMobileMenu] = useState<boolean>(false);
 
-    /**
-     * Handles the mobile menu icon click event.
-     */
-    const openMobileMenuHandler = (): void => {
-        setOpenMobileMenu((prev) => !prev);
-    };
+        const isScrolling = useScrollEffect();
 
-    return (
-        <nav
-            className="w-full top-0 z-50 py-6 relative"
-        >
-            <div className="max-w-[1400px] mx-auto flex justify-between items-center w-[91%]">
-                <Link href={"#home"}>
-                    <h1 className="text-3xl text-[#884DEE] font-bold">Akash S.</h1>
-                </Link>
+        /**
+         * Handles the mobile menu icon click event.
+         */
+        const openMobileMenuHandler = (): void => {
+            setOpenMobileMenu((prev) => !prev);
+        };
 
-                <ul className="flex gap-10 max-md:hidden text-black">
-                    {navLinks.map(({ id, name, route }: { id: number, name: string, route: string }) => (
-                        <Link href={route} key={id} className="hover:text-[#884DEE] transition duration-500">
-                            <li>{name}</li>
-                        </Link>
-                    ))}
-                </ul>
+        return (
+            <nav
+                className={`w-full top-0 z-50 py-5 ${isScrolling ? "fixed top-0 bg-white shadow-lg transition duration-500" : "relative"}`}
+            >
+                <div className="max-w-[1400px] mx-auto flex justify-between items-center w-[91%]">
+                    <Link href={"#home"}>
+                        <h1 className="text-3xl text-[#884DEE] font-bold">Akash</h1>
+                    </Link>
 
-                <div className="md:hidden text-3xl cursor-pointer text-black" onClick={openMobileMenuHandler}>
-                    {openMobileMenu ? <FaTimes /> : <FaBars />}
-                </div>
-
-                {openMobileMenu && (
-                    <ul className="md:hidden bg-[#884DEE] absolute top-14 right-5 px-4 py-6 text-center rounded-md flex flex-col gap-3 shadow-md">
+                    <ul className={`flex gap-10 max-md:hidden ${isScrolling ? "text-black" : "text-white"}`}>
                         {navLinks.map(({ id, name, route }: { id: number, name: string, route: string }) => (
-                            <Link href={route} key={id}>
+                            <Link href={route} key={id} className="hover:text-[#884DEE] transition duration-500">
                                 <li>{name}</li>
                             </Link>
                         ))}
                     </ul>
-                )}
-                {children}
-            </div>
-        </nav>
-    );
-};
+
+                    <div className={`md:hidden text-3xl cursor-pointer ${isScrolling ? "text-[#884DEE]" : "text-white"}`} onClick={openMobileMenuHandler}>
+                        {openMobileMenu ? <FaTimes /> : <FaBars />}
+                    </div>
+
+                    {openMobileMenu && (
+                        <ul className="md:hidden bg-[#884DEE] absolute top-14 right-5 px-4 py-6 text-center rounded-md flex flex-col gap-3 shadow-md">
+                            {navLinks.map(({ id, name, route }: { id: number, name: string, route: string }) => (
+                                <Link href={route} key={id}>
+                                    <li>{name}</li>
+                                </Link>
+                            ))}
+                        </ul>
+                    )}
+                    {children}
+                </div>
+            </nav>
+        );
+    };
 
 export default Navbar;
